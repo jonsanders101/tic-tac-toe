afd3803aba722685bcf0bfee23481faa36147158
Jon Sanders <jonsanders101@aol.com>
2017-11-30 16:37:45 +0000

    Update field value each turn

--- spec/jasmine_examples/GameSpec.js
+++ spec/jasmine_examples/GameSpec.js
@@ -3,9 +3,15 @@ describe("Game", function () {
 
   describe("#play", function () {
     var game = new Game();
-    var mockBoard = {};
+    var mockField = {playField: () => {}};
+    var mockBoard = { fields: [mockField] };
     it("returns an updated board and a game status", function () {
       expect(game.play(0, mockBoard)).toEqual([mockBoard, 'pending']);
+    });
+    it("calls #playField", function () {
+      spyOn(mockField, "playField");
+      game.play(0, mockBoard);
+      expect(mockField.playField).toHaveBeenCalled();
     });
   });
 });
