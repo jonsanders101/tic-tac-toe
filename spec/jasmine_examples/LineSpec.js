6a55577c73b4ddb7b70666c8ceeb0e4ebd7dd8dc
Jon Sanders <jonsanders101@aol.com>
2017-11-30 14:04:42 +0000

    Return true when values in line match

--- spec/jasmine_examples/LineSpec.js
+++ spec/jasmine_examples/LineSpec.js
@@ -8,4 +8,14 @@ describe("Line", function () {
       expect(line.fields).toEqual(["field", "field", "field"]);
     });
   });
+
+  describe('#isWon', function () {
+    it('should return true if each field has the same non-null value', function () {
+      var field0 = {value: "X"};
+      var field1 = {value: "X"};
+      var field2 = {value: "X"};
+      var line = new Line([field0, field1, field2]);
+      expect(line.isWon()).toEqual(true);
+    });
+  });
 });
