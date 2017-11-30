5c4717fb11f04bacde94627ad2eaff6d6548f87b
Jon Sanders <jonsanders101@aol.com>
2017-11-30 14:32:37 +0000

    Allow field value to be updated

--- spec/jasmine_examples/FieldSpec.js
+++ spec/jasmine_examples/FieldSpec.js
@@ -1,14 +1,20 @@
 describe("Field", function () {
   var Field = require('../../lib/Field');
+  var field = new Field(0);
 
   describe('#new', function () {
-    var field = new Field(0);
 
     it('should instantiate with given position reference', function () {
       expect(field.position).toEqual(0);
     });
     it('should instantiate with null value', function () {
       expect(field.value).toEqual(null);
+    });
+  });
+  describe('#playField', function () {
+    it('changes value to mark of player', function () {
+      field.playField("X");
+      expect(field.value).toEqual("X");
     });
   });
 });
