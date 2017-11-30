01be33a395f10a9ecc650daa3ec584569a464ea3
Jon Sanders <jonsanders101@aol.com>
2017-11-30 11:42:00 +0000

    Refactor CellSpec

--- spec/jasmine_examples/CellSpec.js
+++ spec/jasmine_examples/CellSpec.js
@@ -2,13 +2,13 @@ describe("Cell", function () {
   var Cell = require('../../lib/Cell');
 
   describe('#new', function () {
-    it('should instantiate with given position value', function () {
-      var cell = new Cell(0);
+    var cell = new Cell(0);
+
+    it('should instantiate with given position reference', function () {
       expect(cell.position).toEqual(0);
     });
     it('should instantiate with null value', function () {
-      var cell = new Cell(0);
       expect(cell.value).toEqual(null);
-    })
+    });
   });
 });
