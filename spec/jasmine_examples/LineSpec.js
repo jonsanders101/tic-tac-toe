d807192ec9233cad1fbb0853b2bc79fe6f654f47
Jon Sanders <jonsanders101@aol.com>
2017-11-30 14:20:37 +0000

    Refactor lineSpec with setFields helper

--- spec/jasmine_examples/LineSpec.js
+++ spec/jasmine_examples/LineSpec.js
@@ -1,21 +1,27 @@
 describe("Line", function () {
   var Line = require('../../lib/Line');
+  var line, fields;
+
+  function setFields (fieldValues) {
+    fields = [{value: fieldValues[0]}, {value: fieldValues[1]}, {value: fieldValues[2]}];
+    line = new Line(fields);
+  }
 
   describe('#new', function () {
-    var line = new Line(["field", "field", "field"]);
-
     it('should instantiate with three fields', function () {
-      expect(line.fields).toEqual(["field", "field", "field"]);
+      setFields(["X", "X", "X"]);
+      expect(line.fields).toEqual(fields);
     });
   });
 
   describe('#isWon', function () {
     it('should return true if each field has the same non-null value', function () {
-      var field0 = {value: "X"};
-      var field1 = {value: "X"};
-      var field2 = {value: "X"};
-      var line = new Line([field0, field1, field2]);
+      setFields(["X", "X", "X"]);
       expect(line.isWon()).toEqual(true);
+    });
+    it('should return false if values of fields do not match', function () {
+      setFields(["O", "X", "X"]);
+      expect(line.isWon()).toEqual(false);
     });
   });
 });
