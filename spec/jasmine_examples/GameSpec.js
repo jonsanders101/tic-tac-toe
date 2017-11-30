ba06e3a4b43925ab1ef1cc3e5503b56b46a717d9
Jon Sanders <jonsanders101@aol.com>
2017-11-30 18:39:15 +0000

    Ensure game initializes with a board

--- spec/jasmine_examples/GameSpec.js
+++ spec/jasmine_examples/GameSpec.js
@@ -1,10 +1,18 @@
 describe("Game", function () {
   var Game = require('../../lib/Game');
 
+  describe('#new', function () {
+    it('initializes with a new board', function () {
+        var mockBoard = {};
+        var game = new Game(mockBoard);
+        expect(game.board).toEqual(mockBoard);
+    });
+  });
+
   describe("#play", function () {
-    var game = new Game();
-    var mockField = {};
     var mockBoard = { play: () => {}};
+    var game = new Game(mockBoard);
+    var mockField = {};
     it("returns an updated board and a game status", function () {
       spyOn(mockBoard, "play");
       game.play(mockField, mockBoard);
