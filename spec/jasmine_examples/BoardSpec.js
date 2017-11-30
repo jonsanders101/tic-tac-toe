689cc9f87d5c407052c892fb0d42d49cf775db3a
Jon Sanders <jonsanders101@aol.com>
2017-11-30 15:55:21 +0000

    Create lines when board is instantiated

--- spec/jasmine_examples/BoardSpec.js
+++ spec/jasmine_examples/BoardSpec.js
@@ -1,25 +1,32 @@
 describe("Board", function () {
   var Board = require('../../lib/Board');
 
+    function mockField (position) {
+      this.position = position;
+    }
+
+    function mockLine () {
+    }
+
+    function fieldPositions (fields) {
+      var fieldPositions = [];
+      for (fieldCount = 0; fieldCount < fields.length; fieldCount++) {
+        fieldPositions.push(fields[fieldCount].position);
+      }
+      return fieldPositions;
+    }
+
+    var testFields = [0,1,2,3,4,5,6,7,8];
+
+    var board = new Board(mockField, mockLine);
+
   describe("#new", function () {
     it('should create 9 fields with position references', function () {
 
-      function mockField (position) {
-        this.position = position;
-      }
-
-      function fieldPositions (fields) {
-        var fieldPositions = [];
-        for (fieldCount = 0; fieldCount < fields.length; fieldCount++) {
-          fieldPositions.push(fields[fieldCount].position);
-        }
-        return fieldPositions;
-      }
-
-      var testFields = [0,1,2,3,4,5,6,7,8];
-
-      var board = new Board(mockField);
       expect(fieldPositions(board.fields)).toEqual(testFields);
+    });
+    it('creates 8 lines', function () {
+      expect(board.lines.length).toEqual(8);
     });
   });
 });
